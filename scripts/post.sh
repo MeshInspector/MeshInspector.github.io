202677632d1301a8f670a9a4219a6d8ececfecdf
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2024-11-13 17:11:29 +0300

    SEO and analitics updates

--- scripts/post.sh
+++ scripts/post.sh
@@ -12,6 +12,8 @@ URL_PREFIX="${2:-https://meshlib.io/documentation}"
 
 BASE_DIR=$(realpath $(dirname "$0"))
 
+"$BASE_DIR/update_logo_link.sh" "$TARGET_DIR/html" "https://meshlib.io/"
+
 # Conditionally run update_canonical.sh if TARGET_DIR is not "MeshLib/dev"
 if [ "$TARGET_DIR" == "MeshLib" ]; then
   "$BASE_DIR/update_canonical.sh" "$TARGET_DIR/html" "$URL_PREFIX"
