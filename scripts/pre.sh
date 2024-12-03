01c41f5b3ac4dd78c70abdb1be1a693a00781b34
Alexandr Burakov <alexandr.burakov@meshinspector.com>
2024-12-03 15:42:27 +0300

    MIC#5260
    update pre- and post-build script
    small fix

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -8,7 +8,7 @@ TARGET_DIR="${1:-MeshLib}"
 rm -rf "$TARGET_DIR/html"
 
 # generate customizable HTML parts
-doxygen -w html html_header.html html_footer.html html_stylesheet.css Doxyfile
+doxygen -w html html_header.html html_footer.html html_stylesheet.css DoxyfileMain
 
 # inject Google Analytics code into the HTML header
 sed \
@@ -16,8 +16,8 @@ sed \
     -e "/<body>/r scripts/analytics/html_body.html" \
     -i html_header.html
 
-# Additional insertion for html_head_canonical.html if $1 is not "MeshLib/dev"
-if [ "$1" != "MeshLib/dev" ]; then
+# Additional insertion for html_head_canonical.html if $TARGET_DIR is "MeshLib"
+if [ "$TARGET_DIR" == "MeshLib" ]; then
     sed \
         -e "/<head>/r scripts/analytics/html_head_canonical.html" \
         -i html_header.html
@@ -31,19 +31,27 @@ sed -n -e "/<\/head>/,$ p" html_header.html >> html_header.html.tmp
 rm html_header.html
 mv html_header.html.tmp html_header.html
 
-# force Doxygen to use the custom HTML header
 sed \
     -e "s/HTML_HEADER\s*=.*/HTML_HEADER = html_header.html/" \
     -i.bak Doxyfile
-sed -e "s/HTML_HEADER\s*=.*/HTML_HEADER = html_header.html/" -i.bak DoxyfileMain
-sed -e "s/HTML_HEADER\s*=.*/HTML_HEADER = html_header.html/" -i.bak DoxyfileCpp
-sed -e "s/HTML_HEADER\s*=.*/HTML_HEADER = html_header.html/" -i.bak DoxyfilePy
-
-# force Doxygen to use the custom output directory
-if [ $1 ] 
-then
 sed -e "s|OUTPUT_DIRECTORY\s*=.*|OUTPUT_DIRECTORY = $1|" -i Doxyfile
-sed -e "s|OUTPUT_DIRECTORY\s*=.*|OUTPUT_DIRECTORY = $1/main|" DoxyfileMain
-sed -e "s|OUTPUT_DIRECTORY\s*=.*|OUTPUT_DIRECTORY = $1/cpp|" DoxyfileCpp
-sed -e "s|OUTPUT_DIRECTORY\s*=.*|OUTPUT_DIRECTORY = $1/py|" DoxyfilePy
+
+URL=""
+if [ "$TARGET_DIR" == "MeshLib" ]; then
+    URL="meshlib.io/$TARGET_DIR"
+elif [ "$TARGET_DIR" == "MeshLib/dev" ]; then
+    URL="meshinspector.github.io/$TARGET_DIR"
+elif [ "$TARGET_DIR" == "MeshLib" ]; then
+    URL="127.0.0.1:8000/$TARGET_DIR"
 fi
+
+MODULES=(Py Cpp Main)
+for MODULE in ${MODULES[*]}
+do
+    rm -rf ${TARGET_DIR}/${MODULE}/html
+    ../MeshLib/scripts/doxygen/generate_doxygen_layout.sh $MODULE $URL
+    # force Doxygen to use the custom HTML header
+    sed -e "s|HTML_HEADER\s*=.*|HTML_HEADER = html_header.html|" -i.bak Doxyfile${MODULE}
+    # force Doxygen to use the custom output directory
+    sed -e "s|OUTPUT_DIRECTORY\s*=.*|OUTPUT_DIRECTORY = ${TARGET_DIR}/${MODULE}|" -i Doxyfile${MODULE}
+done
