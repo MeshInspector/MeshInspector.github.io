4de8d30d3504639e78555f4578d1a659e459dca6
Grant Karapetyan <grant.karapetyan@meshinspector.com>
2025-03-13 17:10:38 +0300

    Add nofollow attribute to doxygen link

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -31,6 +31,8 @@ cat scripts/doxygen-awesome-scripts.html >> html_header.html.tmp
 sed -n -e "/<\/head>/,$ p" html_header.html >> html_header.html.tmp
 rm html_header.html
 mv html_header.html.tmp html_header.html
+# set nofollow tag in footer
+sed -i "s|href=|rel=\"nofollow\" href=|g" html_footer.html
 
 if [ ! -f ../MeshLib/scripts/doxygen/generate_doxygen_layout.sh ]; then
     echo "[ERROR] Can not found script to generate doxygen layout files"
@@ -43,6 +45,8 @@ do
     ../MeshLib/scripts/doxygen/generate_doxygen_layout.sh $MODULE
     # force Doxygen to use the custom HTML header
     sed -e "s|HTML_HEADER\s*=.*|HTML_HEADER = html_header.html|" -i.bak Doxyfile${MODULE}
+    # force Doxygen to use the custom HTML footer
+    sed -e "s|HTML_FOOTER\s*=.*|HTML_FOOTER = html_footer.html|" -i.bak Doxyfile${MODULE}
     # force Doxygen to use the custom output directory
     sed -e "s|OUTPUT_DIRECTORY\s*=.*|OUTPUT_DIRECTORY = ${TARGET_DIR}|" -i Doxyfile${MODULE}
 done
